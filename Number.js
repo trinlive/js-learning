7684b0e136ed132237a32c1ac751ef356874df69
trinlive <i.trin@live.com>
2020-07-18 21:03:34 +0700

    initial commit

---
+++ Number.js
@@ -0,0 +1,36 @@
+console.log(100, 0, -0, -300, 3.14, -78.222);
+// 200 x 10 raised to the power 10
+console.log(200e5);
+// 2 x 10 raised to the power -5
+console.log(2E-5);
+
+//Take the square root of -1
+console.log(Math.sqrt(-1));
+// 0 divided 0 doesn't have a definition
+console.log(0 / 0);
+// Convert "Hi" to numbers
+console.log(parseInt("Hi"))
+
+/* 
+    Infinity is a value without boundaries. Or there is no way to end in math
+    And in JS, there is a Variable Object Global named "Infinity" which holds that value.
+    and NaN is global object to store the Not-A-Number.
+*/
+console.log(Infinity * Infinity);
+console.log(Infinity / Infinity);
+console.log(-344 * Infinity);
+console.log(3 / Infinity);
+
+
+/* Normally, Number have a scope to display the following values.*/
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_VALUE + 100);
+console.log(Number.MAX_VALUE * 10);
+console.log(Number.MAX_VALUE * -10);
+
+/* 
+    In the Number.MIN_VALUE is 5e-324 which is not the smallest negative value 
+    But is the value of the number closest to 0
+*/
+
+console.log(Number.MIN_VALUE);
