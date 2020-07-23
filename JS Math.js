52432ea5edeffcd954b24665c6bd1286fd8a9d06
trinlive <i.trin@live.com>
2020-07-24 01:46:24 +0700

    new

---
+++ JS Math.js
@@ -0,0 +1,20 @@
+// JS Math Object the JS math object allows you to perform mathematical tasks on numbers.
+console.log(Math.PI); //3.141592653589793
+
+// Math.round() returns the value of x rounded to its nearest integer.
+console.log(Math.round(4.7)); //5
+
+// Math.pow(x, y) returns the value of x to the power of y:
+console.log(Math.pow(8, 2)); //64
+
+// Math.sqrt(x) returns the square root of x:
+console.log(Math.sqrt(64)); //8
+
+// Math.abs(x) returns the absolute (positive) value of x:
+console.log(Math.abs(-4.7)); //4.7
+
+// Math.ceil(x) returns the value of x rounded up to its nearest integer;
+console.log(Math.ceil(4.4)); //5
+
+// Math.floor(x) returns the value of x rounded down to its nearest integer;
+console.log(Math.floor(4.7)); //4
