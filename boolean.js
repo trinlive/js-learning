8f066fda438b1cb0789dfaee03d38b2c98499a30
trinlive <i.trin@live.com>
2020-07-29 21:13:17 +0700

    Create boolean.js

---
+++ boolean.js
@@ -0,0 +1,13 @@
+//Creating Boolean objects with an ititial value of false
+
+var dNoParam = new Boolean();
+var bZero = new Boolean(0);
+var bNull = new Boolean(null);
+var bEmptyString = new Boolean('');
+var bfalse = new Boolean(false);
+
+console.log(dNoParam);  //false
+console.log(bZero);     //false
+console.log(bNull);     //false
+console.log(bEmptyString);  //false
+console.log(bfalse);        //false
