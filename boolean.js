5408fa3ed5ec8cf562ef733a76548f6cb4547f2f
trinlive <i.trin@live.com>
2020-07-29 21:14:59 +0700

    Update boolean.js

--- boolean.js
+++ boolean.js
@@ -5,9 +5,11 @@ var bZero = new Boolean(0);
 var bNull = new Boolean(null);
 var bEmptyString = new Boolean('');
 var bfalse = new Boolean(false);
+var btrue = new Boolean(true);
 
 console.log(dNoParam);  //false
 console.log(bZero);     //false
 console.log(bNull);     //false
 console.log(bEmptyString);  //false
 console.log(bfalse);        //false
+console.log(btrue);         //true
