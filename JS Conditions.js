2077e833aeba44c0900a1744f24a056d6ba2e094
trinlive <i.trin@live.com>
2020-08-03 23:49:16 +0700

    new

---
+++ JS Conditions.js
@@ -0,0 +1,12 @@
+
+x = new Date().getHours();
+console.log(x);
+console.log(typeof (x));
+
+if (new Date().getHours() > 99) {
+    console.log('Good day');
+
+} else {
+    console.log("Good evening");
+}
+
