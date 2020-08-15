206aba568b752e11ad240478a7a8904fc21ebdaf
trinlive <i.trin@live.com>
2020-08-15 23:26:13 +0700

    new

---
+++ Array.js
@@ -0,0 +1,14 @@
+/* 
+    array.map
+    The map() method creates a new array with the results of calling a provided function on every
+    element in the calling array.
+*/
+
+
+const array1 = [1, 4, 9, 16];
+
+// pass a function to map
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
+// expected output: Array [2, 8, 28, 32]
